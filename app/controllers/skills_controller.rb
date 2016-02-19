49cae2484cb0aa0dbfe60050756cd5335c44fb5c
dianacamacho <dcam1901@yahoo.com>
2016-02-18 21:08:24 -0600

    added skills and capstone routes and controller actions

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,31 @@
+class SkillsController < ApplicationController
+
+  def index
+    @skills = Skill.all
+  end
+
+  def create
+    @skill = Skill.create(skill_name: params[:skill_name])
+
+    render :show
+  end
+
+  def show
+    @skill = Skill.find(params[:id])
+  end
+
+  def update
+    @skill = Skill.find(params[:id])
+    @skill.update(skill_name: params[:skill_name])
+
+    render :show
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+
+    render json: {message: "Skill deleted"}
+  end
+
+end
