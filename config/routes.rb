443a7a9a6a235398c8d6a847d92c997fc2fb8412
Bobbie <obobiec@gmail.com>
2016-02-22 18:26:44 -0600

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get '/students' => 'students#index'
-  get '/students/:id/new' => 'students#new'
-  post '/students' => 'students#create'
   
+  get '/students/new' => 'students#new'
+  post '/students' => 'students#create'
+
   get '/students/:id' => 'students#show'
 
   get '/students/:id/edit' => 'students#edit'
